38132ee3d3407f30ddb8974b09a2d52cd4846c85
Tiago Murakami <trmurakami@gmail.com>
2016-10-01 17:31:10 -0300

    Arquivos iniciais

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
             include('inc/functions.php');
             include('inc/meta-header.php');
         ?>
+        <title>Repertório da Produção Periódica de Biblioteconomia e Ciência da Informação - RPPBCI</title>
     </head>    
     <body>
+        <div class="uk-container uk-container-center uk-margin-large-top">
+            <h1>RPPBCI</h1>
+        </div>
     </body>
 </html>
