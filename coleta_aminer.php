08695d9f3e00f9b30f1aa06bbe7b386dc8319c5f
Tiago Murakami <trmurakami@bdpife2.sibi.usp.br>
2017-10-30 09:31:15 -0200

    Verificação de título

--- coleta_aminer.php
+++ coleta_aminer.php
@@ -29,23 +29,39 @@
             unset($update_aminer);
         } else {           
             $aminer = metrics::get_aminer($r["_source"]["titulo"]);
+            print_r($r["_source"]["titulo"]);
+            echo "<br/>";
             if(count($aminer["result"]) > 0 ){
                 similar_text($r["_source"]["titulo"], $aminer["result"][0]["title"], $percent);
                 echo 'Percentual de: '.$percent.'';
-                if ($percent > 90) {                            
-                    $update_aminer["doc"]["aminer"] = $aminer["result"][0];
-                    $update_aminer["doc"]["aminer"]["date"] = date("Ymd");
-                    $update_aminer["doc_as_upsert"] = true;
-                    $result_aminer = elasticsearch::elastic_update($r['_id'],$type,$update_aminer);
-                    print_r($result_aminer);
-                    unset($update_aminer);
-                } else {
-                $update_aminer["doc"]["aminer"]["date"] = date("Ymd");
-                $update_aminer["doc_as_upsert"] = true;
-                $result_aminer = elasticsearch::elastic_update($r['_id'],$type,$update_aminer);
-                print_r($result_aminer);
-                unset($update_aminer);
-                }
+                if ($percent > 90) {
+                    //print_r($aminer);
+                    if (!empty($aminer["result"][0]["venue"]["name"])) {
+                        similar_text($r["_source"]["source"], $aminer["result"][0]["venue"]["name"], $percent_source);
+                        echo 'Percentual do Título do periódico de: '.$percent_source.'';
+                        if ($percent_source > 90) {
+                            $update_aminer["doc"]["aminer"] = $aminer["result"][0];
+                            $update_aminer["doc"]["aminer"]["date"] = date("Ymd");
+                            $update_aminer["doc_as_upsert"] = true;
+                            $result_aminer = elasticsearch::elastic_update($r['_id'],$type,$update_aminer);
+                            print_r($result_aminer);
+                            unset($update_aminer);
+                        } else {
+                            $update_aminer["doc"]["aminer"]["date"] = date("Ymd");
+                            $update_aminer["doc_as_upsert"] = true;
+                            $result_aminer = elasticsearch::elastic_update($r['_id'],$type,$update_aminer);
+                            print_r($result_aminer);
+                            unset($update_aminer);
+                        } 
+                    } else {
+                        $update_aminer["doc"]["aminer"] = $aminer["result"][0];
+                        $update_aminer["doc"]["aminer"]["date"] = date("Ymd");
+                        $update_aminer["doc_as_upsert"] = true;
+                        $result_aminer = elasticsearch::elastic_update($r['_id'],$type,$update_aminer);
+                        print_r($result_aminer);
+                        unset($update_aminer);
+                    }
+
             } else {
                 $update_aminer["doc"]["aminer"]["date"] = date("Ymd");
                 $update_aminer["doc_as_upsert"] = true;
@@ -60,6 +76,7 @@
         //sleep(5);
 
 
+        }
     }   
     
 
