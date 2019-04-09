31d0a697fdb41b988c543108666fd52edc913cc9
Tiago <tiago@bdpife2.sibi.usp.br>
2019-04-09 13:46:26 -0300

    About text

--- about.php
+++ about.php
@@ -11,18 +11,37 @@
     <body>
         <div class="uk-container uk-container-center uk-margin-top uk-margin-large-bottom">
             <?php require 'inc/navbar.php' ?>
-            
-            <h2>Sobre o RPPBCI</h2>
-            
-            <p>O Repertório da Produção Periódica Brasileira de Ciência da Informação (RPPBCI) é um coletânea de metadados de artigos de periódicos de Ciência da Informação disponibilizados em OAI-PMH.</p>
 
-            <p>Está sendo desenvolvido no <a href="http://dgp.cnpq.br/dgp/espelhogrupo/5207019450670905">Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics)</a> na linha de pesquisa Ciência 2.0 e os aportes da altmetria por Tiago Rodrigo Marçal Murakami do Sistema Integrado de Bibliotecas da Universidade de São Paulo, em parceria com o Prof. Dr. Ronaldo Ferreira de Araujo do Curso de BIblioteconomia do Instituto de Ciências Humanas, Comunicação e Artes da Universidade Federal de Alagoas (CB/ICHCA/UFAL)</p>
-            
-            <h2>Entre em contato</h2>
-            
-            <p>Tiago Rodrigo Marçal Murakami ( tiago.murakami EM dt.sibi.usp.br )</p>
+            <h2>Sobre o Portal Periódicos Científicos Alagoas</h2>
 
-            <p>Prof. Ronaldo Ferreira de Araújo ( ronaldfa EM gmail.com )</p>
+            <p>O Portal Periódicos de Alagoas é um serviço de descoberta desenvolvimento no Grupo de Pesquisa: <a href="http://dgp.cnpq.br/dgp/espelhogrupo/205221">“Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics/UFAL)”</a> contemplado no Edital 04/2016 Chamada Universal da Fundação de Amparo à pesquisa de Alagoas (FAPEAL), pelo processo nº 600301057/2016, no âmbito do Projeto: “Atenção online e métricas alternativas da produção científica registrada em artigos de periódicos do Estado de Alagoas: áreas de conhecimento, instituições e pesquisadores”.</p>
+
+            <p>O projeto é coordenado pelo Prof. Ronaldo Ferreira de Araújo do Curso de Biblioteconomia do Instituto de Ciências Humanas, Comunicação e Artes da Universidade Federal de Alagoas (CB/ICHCA/UFAL) com colaboração do Tiago Rodrigo Marçal Murakami do Sistema Integrado de Bibliotecas da Universidade de São Paulo (USP) a participação de bolsistas e colaboradores de iniciação científica e de desenvolvimento tecnológico.</p>
+
+            <p>Bolsistas/Colaboradores do Projeto:</p>
+
+            <p>Programa Institucional de Bolsas de Iniciação Científica (PIBIC/UFAL) 2017-2018:</p>
+            <ul>    
+            <li>Carlina Rocha Leite (Biblioteconomia / UFAL)</li>
+            <li>Laysa Lorena Alves de Araujo (Biblioteconomia / UFAL)</li>
+            <li>Patricia Pedri (Biblioteconomia / UFAL)</li>
+            <li>Stephanie Silva Menezes (Biblioteconomia / UFAL)</li>
+            </ul>
+
+            <p>Programa Institucional de Bolsas de Iniciação em Desenvolvimento Tecnológico e Inovação (PIBITI/UFAL) 2018-2019: </p>
+            <ul> 
+            <li>Erycles Da Silva Santos (Ciência da Computação / UFAL)</li> 
+            <li>Laysa Lorena Alves de Araujo (Biblioteconomia / UFAL) </li>
+            <li>Richard Bryan Gomes Cavalcante (Comunicação Social - Jornalismo / UFAL) </li>
+            </ul>
+
+            <p>Contato:</p>
+
+            <p>Tel.: (82) 3214-1479</p>
+
+            <p>E-mail: ronaldo.araujo@ichca.ufal.br</p>
+            
+            </p>
             
             <?php require 'inc/offcanvas.php'?>
         </div>    
