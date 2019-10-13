0b16f83b4dc1936be1a832457b1a2c5112d536c0
Tiago Murakami <trmurakami@gmail.com>
2019-10-13 10:27:30 -0300

    Initial

--- inc/functions.php
+++ inc/functions.php
@@ -1050,10 +1050,12 @@ class Homepage
                             $autArray[] = '<a href="result.php?filter[]=author.person.name:&quot;'.$autores["person"]["name"].'&quot;">'.$autores["person"]["name"].'</a>';
                         }
                         echo '<p class="card-text"><small class="text-muted">'.implode(" | ", $autArray).'</small></p>';
+                        unset($autArray);
                     };
                     if (!empty($r["_source"]['facebook'])) {
                         echo '<p class="card-text"><small class="text-muted">Total de interações no Facebook: '.$r["_source"]['facebook']['facebook_total'].'</small></p>';
-                    };                    
+                    };
+                                        
 
                     echo '
                     </div></div>
