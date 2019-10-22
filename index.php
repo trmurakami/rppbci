b0e6bba9b5f2777552fc9ab810e4bdd9d5cd0825
Tiago Murakami <trmurakami@gmail.com>
2019-10-22 18:47:03 -0300

    Initial

--- index.php
+++ index.php
@@ -69,19 +69,14 @@ require 'inc/functions.php';
             
         </form>
         <br/><br/>
-        <div class="row">
-      <!--
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div>
-      -->
+
+        </div>
+    </div>
+
+  <div class="container">
+    <!-- Example row of columns -->
+     
+    <div class="row">
         <div class="col-md-4">
           <h2>Periódicos indexados</h2>
           <ul class="list-group">
@@ -101,17 +96,6 @@ require 'inc/functions.php';
           </ul>
         </div> 
       </div>
-
-        <br/><br/><br/><br/>
-
-
-        </div>
-    </div>
-
-  <div class="container">
-    <!-- Example row of columns -->
-     
-    
 
     <hr>
     <h1>Registros com mais interações</h1>
