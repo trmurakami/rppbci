70e6e4c799dfae8ddafd8ac60b219c0798374fee
Tiago Murakami <trmurakami@gmail.com>
2019-10-23 16:36:57 -0300

    Initial

--- inc/config.example.php
+++ inc/config.example.php
@@ -22,8 +22,8 @@ $client = \Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build();
 $fb = new Facebook\Facebook([
   'app_id' => '',
   'app_secret' => '',
-  'default_graph_version' => 'v2.9',
-  'http_client_handler' => 'guzzle'
+  'default_graph_version' => 'v2.10',
+  'http_client_handler' => 'stream'
 ]);
 
 // Sets the default fallback access token so we don't have to pass it to each request
