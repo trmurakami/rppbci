c403dad8a0da321530b4401b9fc41b57345554a1
Tiago Murakami <trmurakami@gmail.com>
2019-10-23 12:56:02 -0300

    Initial

--- tools/crossref.php
+++ tools/crossref.php
@@ -25,24 +25,25 @@ foreach ($cursor["hits"]["hits"] as $r) {
     print("<pre>".print_r($r, true)."</pre>");
 
     $clientCrossref = new RenanBr\CrossRefClient();
-    $clientCrossref->setUserAgent('GroovyBib/1.1 (https://bdpi.usp.br/; mailto:tiago.murakami@dt.sibi.usp.br)');
+    $clientCrossref->setUserAgent('GroovyBib/1.1 (http://35.239.2.201/rppbci/; mailto:trmurakami@gmail.com)');
     $exists = $clientCrossref->exists('works/'.$r["_source"]["doi"].'');
     var_dump($exists);
     if ($exists == true) {
         $work = $clientCrossref->request('works/'.$r["_source"]["doi"].'');
+        print("<pre>".print_r($work, true)."</pre>");
         echo "<br/><br/><br/><br/>";
         $body["doc"]["USP"]["crossref"] = $work;
         $body["doc_as_upsert"] = true;
-        $resultado_crossref = elasticsearch::store_record($r["_id"], $type, $body);
-        print_r($resultado_crossref);
+        //$resultado_crossref = Elasticsearch::update($r["_id"], $body);
+        //print_r($resultado_crossref);
         sleep(11);
         ob_flush();
         flush();          
     } else {
         $body["doc"]["USP"]["crossref"]["notFound"] = true;
         $body["doc_as_upsert"] = true;
-        $resultado_crossref = elasticsearch::store_record($r["_id"], $type, $body);
-        print_r($resultado_crossref);
+        //$resultado_crossref = Elasticsearch::update($r["_id"], $body);
+        //print_r($resultado_crossref);
         sleep(2);
         ob_flush();
         flush();
