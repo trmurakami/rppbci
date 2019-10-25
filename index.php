4b6c95275e6eab0f110a883a38007470bc01b7b6
Tiago Murakami <trmurakami@gmail.com>
2019-10-25 10:30:05 -0300

    Initial

--- index.php
+++ index.php
@@ -64,6 +64,8 @@ require 'inc/functions.php';
                 <label for="searchQuery">Termos de busca</label>
                 <input type="text" name="search" class="form-control" id="searchQuery" aria-describedby="searchHelp" placeholder="Pesquise por termo ou autor">
                 <small id="searchHelp" class="form-text text-muted">Dica: Use * para busca por radical. Ex: biblio*.</small>
+                <small id="searchHelp" class="form-text text-muted">Dica 2: Para buscas exatas, coloque entre ""</small>
+                <small id="searchHelp" class="form-text text-muted">Dica 3: Use operadores booleanos: AND, OR</small>
             </div>                       
             <button type="submit" class="btn btn-primary">Pesquisar</button>
             
