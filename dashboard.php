45bb9f50f05cce2f01393f36414756d8a973b071
Tiago Murakami <trmurakami@gmail.com>
2020-06-23 11:45:26 -0300

    Initial

--- dashboard.php
+++ dashboard.php
@@ -17,6 +17,13 @@
             
             <h2>Estatísticas</h2>
 
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Para login</strong> use o <strong>usuário: dashboard</strong> e <strong>senha: dashboard</strong>.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
             <?php if (isset($dashboard)) : ?>
                 <iframe src="<?php echo $dashboard ?>" height="10000" width="100%" frameBorder="0" scrolling="no"></iframe>
             <?php endif ?>
